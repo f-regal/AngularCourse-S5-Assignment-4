8757dbb142c8bc9b70c19473fe48fad9f905fa0d
Faysal Regal <faysalregal@gmail.com>
2022-09-23 21:27:42 +0100

    used prop and input decor to pass number prop down

--- src/app/game-control/even/even.component.ts
+++ src/app/game-control/even/even.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class EvenComponent implements OnInit {
 
-@Input() number: any;
+@Input() number : any
 constructor() { }
 
 ngOnInit(): void {
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -8,22 +8,22 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class GameControlComponent implements OnInit {
   @Output() numberIncremented = new EventEmitter<{number: number}>();
   number = 0;
-
+  inter: any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 
-  startIncrement() {
-    setInterval(()=>{
+  startIncrement(){
+      this.inter = setInterval(()=>{
       this.number += 1;
       this.numberIncremented.emit({number: this.number})
     }, 1000);
   }
 
   stopIncrement() {
-    
+    clearInterval(this.inter);
   }
 
 }
